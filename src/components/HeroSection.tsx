dd5595454acbfe26a9289f4b4f788d73d1ce5a6f
chinthanachamod <chinthanachamod50@gmail.com>
2025-10-05 15:32:18 +0530

    commit

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,4 +1,3 @@
-import React, { Children } from 'react';
 import { motion } from 'framer-motion';
 import { ArrowDownIcon, FileDownIcon } from 'lucide-react';
 import { useTheme } from './ThemeContext';
